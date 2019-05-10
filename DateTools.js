ec83440a180c849501b96d3b165e58668aff16ea
Willian Froes <42048301+willian-froes@users.noreply.github.com>
2019-05-09 22:03:01 -0300

    Update DateTools.js

--- DateTools.js
+++ DateTools.js
@@ -1,12 +1,19 @@
 class DateTools {
     
-    //in this function you have to add maxlength="10" and onkeyup="Date-tools.format(this.value)" atribute on html text input,
+    //in this function you have to add maxlength="10" and onkeyup="Date-tools.formatToUser(this.value)" atribute on html text input,
     //or use data-mask="__/__/____"
-    
-    static format(value) {
+    static formatToUser(value) {
         var value = value;
 
         if(value.length == 2 || value.length == 5) value += "/";
-        document.querySelector("#data").value = value;
+        document.querySelector("#date).value = value;
+    }
+    
+    //it's equals the first function, but returns date in system format
+    static formatToSystem(value) {
+        var value = value;
+           2019-12-10
+        if(value.length == 4 || value.length == 7) value += "-";
+        document.querySelector("#date).value = value;
     }
 }
